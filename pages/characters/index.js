cbf115326f763df556503b92cedb579a2622ae52
Jesse Songok <jessesongok@gmail.com>
2022-03-27 21:04:42 +0300

    Refactored Characters Component to fetch data from API

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,7 +1,21 @@
 import styles from "../../styles/Book.module.css";
 import Head from "next/head";
 import Image from "next/image";
-function Characters() {
+// This function gets called at build time
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts
+    const res = await fetch('https://topupmama-backend.herokuapp.com/api/v1/characters')
+    const characters = await res.json()
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            characters,
+        },
+    }
+}
+function Characters({characters}) {
     return (
         <div className={styles.container}>
             <Head>
@@ -14,6 +28,19 @@ function Characters() {
                 <h1 className={styles.title}>
                     Characters
                 </h1>
+                Character Totals: {characters.character_totals}
+
+                {characters.character_data.map((character) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <div className={styles.card}>
+                        Url:  {character.url} <br/>
+                        Name: {character.name} <br/>
+                        Gender: {character.gender} <br/>
+                        Culture: {character.culture} <br/>
+                        Birth Date: {character.born}
+                    </div>
+                ))}
+
 
             </main>
             <footer className={styles.footer}>
